2c567e85ee520042077fd56410d39fdf7e936798
Nathan Marley <nathan.marley@gmail.com>
2024-01-08 13:41:12 -0600

    stage

--- src/main.rs
+++ src/main.rs
@@ -18,42 +18,62 @@ fn main() {
     {
         let file_contents = fs::read_to_string(entry.path()).unwrap();
         let file_lines: Vec<&str> = file_contents.split('\n').collect();
-        // println!("{}", entry.path().display());
         if file_lines.len() <= feed_limit {
-            // buf.push()
-            print_entire_file(entry.path(), &file_lines, feed_limit);
+            buf.push(print_entire_file(entry.path(), &file_lines, feed_limit));
         } else {
-            print_as_parts(entry.path(), &file_lines, feed_limit);
+            buf.push(print_as_parts(entry.path(), &file_lines, feed_limit));
         }
     }
+
+    // now output is gathered
 }
 
-fn print_as_parts<P: AsRef<Path>>(filename: P, lines: &[&str], feed_limit: usize) {
+const TRIPLE_BACKTICK: &str = "```";
+
+fn print_as_parts<P: AsRef<Path>>(filename: P, lines: &[&str], feed_limit: usize) -> Vec<String> {
+    let mut buf: Vec<String> = Vec::new();
     let mut lines_printed: usize = 0;
     let mut count_parts_printed: usize = 0;
 
     while lines_printed < lines.len() {
-        println!(
+        buf.push(format!(
             "\nPart {} of file `{}`:",
             count_parts_printed + 1,
             filename.as_ref().display()
-        );
+        ));
 
         // print feed_limit lines at a time
         let start = feed_limit * count_parts_printed;
         // but consider if there are less than feed_limit lines left
         let end = core::cmp::min(lines.len(), feed_limit * (count_parts_printed + 1));
-        println!("\n```{}```", lines[start..end].join("\n"));
+        buf.push(TRIPLE_BACKTICK.to_string());
+        // buf.extend_from_slice(lines[start..end]);
+        buf.extend(lines[start..end].iter().map(|s| s.to_string()));
+        buf.push(TRIPLE_BACKTICK.to_string());
         lines_printed += end - start;
         count_parts_printed += 1;
     }
-    println!(
+    buf.push(format!(
         "\nthat concludes the content of file `{}`.",
         filename.as_ref().display()
-    );
+    ));
+    buf
 }
 
-fn print_entire_file<P: AsRef<Path>>(filename: P, lines: &[&str], _feed_limit: usize) {
-    println!("\nContents of file `{}`:", filename.as_ref().display());
-    println!("\n```{}```", lines.join("\n"));
+fn print_entire_file<P: AsRef<Path>>(
+    filename: P,
+    lines: &[&str],
+    _feed_limit: usize,
+) -> Vec<String> {
+    let mut buf: Vec<String> = Vec::new();
+    buf.push(format!(
+        "\nContents of file `{}`:",
+        filename.as_ref().display()
+    ));
+
+    buf.push(TRIPLE_BACKTICK.to_string());
+    buf.extend(lines.iter().map(|s| s.to_string()));
+    buf.push(TRIPLE_BACKTICK.to_string());
+
+    buf
 }
